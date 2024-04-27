b33d0ee52fc24e20dcaf1c6e800b9effa7996840
Vladislav <vgrechannik@gmail.com>
2024-04-27 23:33:50 +0200

    Make LSP feature detection stable-compatible

--- init.lua
+++ init.lua
@@ -526,7 +526,11 @@ require('lazy').setup({
           --
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          -- FIXME: Change this ugly stuff after Neovim 0.10 release
+          local _, supports_document_highlight =
+            vim.lsp.protocol.Methods and pcall(client.supports_method, vim.lsp.protocol.Methods.textDocument_documentHighlight) or false,
+            client.server_capabilities.documentHighlightProvider
+          if client and supports_document_highlight then
             local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
@@ -541,11 +545,15 @@ require('lazy').setup({
             })
           end
 
+          -- FIXME: Change this ugly stuff after Neovim 0.10 release
+          local _, has_inlay_hints =
+            vim.lsp.protocol.Methods and pcall(client.supports_method, vim.lsp.protocol.Methods.textDocument_inlayHint) or false,
+            client.server_capabilities.inlayHintProvider
           -- The following autocommand is used to enable inlay hints in your
           -- code, if the language server you are using supports them
           --
           -- This may be unwanted, since they displace some of your code
-          if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) and vim.lsp.inlay_hint then
+          if client and vim.lsp.inlay_hint and has_inlay_hints then
             map('<leader>th', function()
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
             end, '[T]oggle Inlay [H]ints')
