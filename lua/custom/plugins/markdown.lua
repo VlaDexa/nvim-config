84ff6fd72d5fc5a9a1c40740bc468172d3058cef
Vladislav <vgrechannik@gmail.com>
2024-06-07 11:58:53 +0200

    Add markdown editing goodies

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,10 @@
+return {
+  { 'ellisonleao/glow.nvim', config = true, cmd = 'Glow' },
+  {
+    'tadmccorkle/markdown.nvim',
+    ft = 'markdown', -- or 'event = "VeryLazy"'
+    opts = {
+      -- configuration here or empty for defaults
+    },
+  },
+}
