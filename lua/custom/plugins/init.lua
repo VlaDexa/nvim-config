a4b72128dc1d1959b1265b328d4bc727d494ee93
Vladislav <vgrechannik@gmail.com>
2024-06-09 01:34:17 +0200

    Update lint enabler to use normal detection

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -6,11 +6,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    -- FIXME: Change this ugly stuff after Neovim 0.10 release
-    local _, has_inlay_hints =
-      vim.lsp.protocol.Methods and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) or false,
-      client.server_capabilities.inlayHintProvider
-    if client and vim.lsp.inlay_hint and has_inlay_hints then
+    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
       vim.lsp.inlay_hint.enable(true)
     end
   end,
