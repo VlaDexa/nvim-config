fca1481f20f72ac641fe6bb3944ad63b5211ab9e
Vladislav <vgrechannik@gmail.com>
2024-06-08 22:41:41 +0200

    Follow style from init.lua

--- lua/custom/plugins/file-browser.lua
+++ lua/custom/plugins/file-browser.lua
@@ -1,4 +1,7 @@
 return {
-  'nvim-telescope/telescope-file-browser.nvim',
-  dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  'nvim-telescope/telescope.nvim',
+  dependencies = {
+    'nvim-telescope/telescope-file-browser.nvim',
+    'nvim-lua/plenary.nvim',
+  },
 }
