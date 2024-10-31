db660955c2e759e5af0e6a06a23c830c2f28d3c4
Vladislav <vgrechannik@gmail.com>
2024-10-31 11:38:15 +0100

    Add TSX linting

--- init.lua
+++ init.lua
@@ -891,9 +891,11 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         javascript = { 'prettierd', 'prettier', stop_after_first = true },
         typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
         rust = { 'rustfmt' },
         cpp = { 'clang-format' },
         elm = { 'elm_format', quiet = true },
+        asm = { 'asmfmt' },
       },
     },
     init = function()
