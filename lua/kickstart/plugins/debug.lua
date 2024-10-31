818fb3abd4601588759a82ffabc61c64b96029d5
Vladislav <vgrechannik@gmail.com>
2024-10-31 11:38:36 +0100

    idk asm debugging maybe

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -19,7 +19,23 @@ return {
 
       -- You can provide additional configuration to the handlers,
       -- see mason-nvim-dap README for more information
-      handlers = {},
+      handlers = {
+        asm = function(config)
+          config.configurations = {
+            {
+              name = 'Launch',
+              type = 'codelldb',
+              request = 'launch',
+              program = function()
+                return vim.fn.input 'Path to executable: '
+              end,
+              cwd = '${workspaceFolder}',
+              stopOnEntry = true,
+            },
+          }
+          require('mason-nvim-dap').default_setup(config) -- don't forget this!
+        end,
+      },
 
       -- You'll need to check that you have the required things installed
       -- online, please don't ask me how to install them :)
@@ -86,6 +102,18 @@ return {
       },
     },
     config = function()
+      require('dap').configurations.asm = {
+        {
+          name = 'Launch',
+          type = 'codelldb',
+          request = 'launch',
+          program = function()
+            return vim.fn.input 'Path to executable: '
+          end,
+          cwd = '${workspaceFolder}',
+          stopOnEntry = true,
+        },
+      }
       require('dap').listeners.before.attach.dapui_config = function()
         require('dapui').open()
       end
